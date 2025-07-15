7902605ab2b9a4497cd098ad0cd9b92df8181836
Khalid Al-Ammar <kh.alammar97@gmail.com>
2025-07-15 18:07:09 +0300

    Add variables with sensitive values in .tfvars (ignored by git)

--- variables.tf
+++ variables.tf
@@ -1,9 +1,8 @@
-terraform {
- variable "admin_username" {
+
+variable "admin_username" {
     description = "The administrator username for the virtual machine."
     type        = string
     }
-}
 variable "admin_password" {
     description = "The administrator password for the virtual machine."
     type        = string
